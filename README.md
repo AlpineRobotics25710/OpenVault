40faafc689e5065a48934f77cf2b71c62a0e9868
PrathyushY <prathyushyeturi@gmail.com>
2025-01-03 21:44:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# OpenVault
+
+This is the repository for **OpenVault**, a website developed by FIRST Tech Challenge Team #25710 Alpine Robotics.
+The goal of this website is to make finding open source projects easy to find and use. Instead of having to spend 
+hours scouring the internet or scrolling through thousands of messages, everything can be found here!
+
+Contact us at <a href="mailto:alpineroboticsftc@gmail.com">alpineroboticsftc@gmail.com</a> with anything!
---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():  # put application's code here
+    return render_template("index.html")
+
+@app.route('/cad')
+def cad():
+    return render_template("cad.html")
+
+@app.route('/code')
+def code():
+    return render_template("code.html")
+
+@app.route('/portfolios')
+def portfolios():
+    return render_template("portfolios.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
