236be693878d09aa9b2b3def2f757859460019f7
Hypothetical <adi.varshney09@gmail.com>
2025-01-04 23:43:47 -0500

    Updated all the pages with new list for cad section, added template code to them, fixed and added the new dropdown menu list(navbar), added all necessary @app.routes in app.py, and fixed links - Adi

--- app.py
+++ app.py
@@ -7,21 +7,53 @@ app = Flask(__name__)
 def index():
     return render_template("ftc/index.html")
 
-@app.route('/cad/intake')
-def cad_intake_page():
-    return render_template("ftc/cad/intake.html")
+#      **CAD section**
 
-@app.route('/cad/outtake')
-def cad_outtake_page():
-    return render_template("ftc/cad/outtake.html")
-
-@app.route('/cad/claw')
-def cad_claw_page():
-    return render_template("ftc/cad/claw.html")
-
-@app.route('/cad/slides')
+@app.route('/cad/drivetrains')
 def cad_slides_page():
-    return render_template("ftc/cad/slides.html")
+    return render_template("ftc/cad/drivetrains.html")
+
+@app.route('/cad/passiveIntakes')
+def cad_intake_page():
+    return render_template("ftc/cad/passiveIntakes.html")
+
+@app.route('/cad/activeIntakes')
+def cad_claw_page():
+    return render_template("ftc/cad/activeIntakes.html")
+
+@app.route('/cad/transfers')
+def cad_slides_page():
+    return render_template("ftc/cad/drivetrains.html")
+
+@app.route('/cad/outtakes')
+def cad_outtake_page():
+    return render_template("ftc/cad/outtakes.html")
+
+@app.route('/cad/arms')
+def cad_slides_page():
+    return render_template("ftc/cad/arms.html")
+
+@app.route('/cad/linearMotionGuides')
+def cad_slides_page():
+    return render_template("ftc/cad/linearMotionGuides.html")
+
+@app.route('/cad/deadWheels')
+def cad_slides_page():
+    return render_template("ftc/cad/deadWheels.html")
+
+@app.route('/cad/linkages')
+def cad_slides_page():
+    return render_template("ftc/cad/linkages.html")
+
+@app.route('/cad/powerTransmissions')
+def cad_slides_page():
+    return render_template("ftc/cad/powerTransmissions.html")
+
+@app.route('/cad/turrets')
+def cad_slides_page():
+    return render_template("ftc/cad/turrets.html")
+
+#      **Code section**
 
 @app.route('/code/teleop')
 def code_teleop_page():
