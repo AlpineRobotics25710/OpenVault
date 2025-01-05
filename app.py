51cface77e5a0300fa73ae064aec8bc9259bf2fd
PrathyushY <prathyushyeturi@gmail.com>
2025-01-04 21:44:40 -0500

    Put in all of the pages (for now), added some template code into them, and got all of the url mapping done

--- app.py
+++ app.py
@@ -5,11 +5,39 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("ftc/index.html")
 
 @app.route('/cad/intake')
+def cad_intake_page():
+    return render_template("ftc/cad/intake.html")
+
+@app.route('/cad/outtake')
+def cad_outtake_page():
+    return render_template("ftc/cad/outtake.html")
+
+@app.route('/cad/claw')
+def cad_claw_page():
+    return render_template("ftc/cad/claw.html")
+
+@app.route('/cad/slides')
+def cad_slides_page():
+    return render_template("ftc/cad/slides.html")
+
+@app.route('/code/teleop')
+def code_teleop_page():
+    return render_template("ftc/code/teleop.html")
+
+@app.route('/code/auton')
+def code_auton_page():
+    return render_template("ftc/code/auton.html")
+
+@app.route('/code/ftclib')
+def code_ftclib_page():
+    return render_template("ftc/code/ftclib.html")
+
+@app.route('/portfolios')
 def portfolios():
-    return render_template("cad/intake.html")
+    return render_template("ftc/portfolios/portfolios.html")
 
 
 if __name__ == '__main__':
