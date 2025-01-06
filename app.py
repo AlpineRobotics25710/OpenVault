a4e7ab5b6aacae3de19719b1da7c3785cf0b8e2f
PrathyushY <prathyushyeturi@gmail.com>
2025-01-05 20:52:09 -0500

    Updated app.py and initialized the portfolios database. Need to modify the code database so that it has the right columns

--- app.py
+++ app.py
@@ -50,49 +50,60 @@ def cad_drivetrains_page():
 
 @app.route('/cad/linear-motion-guides')
 def cad_linear_motions_guides_page():
-    return render_template("ftc/cad/linear-motions-guides.html")
+    records = fetch_data_from_db("cad", "linear_motion_guides")
+    return render_template("ftc/cad/linear-motions-guides.html", records=records)
 
 @app.route('/cad/linkages')
 def cad_linkages_page():
-    return render_template("ftc/cad/linkages.html")
+    records = fetch_data_from_db("cad", "linkages")
+    return render_template("ftc/cad/linkages.html", records=records)
 
 @app.route('/cad/outtakes')
 def cad_outtakes_page():
-    return render_template("ftc/cad/outtakes.html")
+    records = fetch_data_from_db("cad", "outtakes")
+    return render_template("ftc/cad/outtakes.html", records=records)
 
 @app.route('/cad/passive-intakes')
 def cad_passive_intakes_page():
-    return render_template("ftc/cad/passive-intakes.html")
+    records = fetch_data_from_db("cad", "passive_intakes")
+    return render_template("ftc/cad/passive-intakes.html", records=records)
 
 @app.route('/cad/power-transmissions')
 def cad_power_transmissions_page():
-    return render_template("ftc/cad/power-transmissions.html")
+    records = fetch_data_from_db("cad", "power_transmissions")
+    return render_template("ftc/cad/power-transmissions.html", records=records)
 
 @app.route('/cad/transfers')
 def cad_transfers_page():
-    return render_template("ftc/cad/transfers.html")
+    records = fetch_data_from_db("cad", "transfers")
+    return render_template("ftc/cad/transfers.html", records=records)
 
 @app.route('/cad/turrets')
 def cad_turrets_page():
-    return render_template("ftc/cad/turrets.html")
+    records = fetch_data_from_db("cad", "turrets")
+    return render_template("ftc/cad/turrets.html", records=records)
 
 #      **Code section**
 
 @app.route('/code/teleop')
 def code_teleop_page():
-    return render_template("ftc/code/teleop.html")
+    records = fetch_data_from_db("code", "teleop")
+    return render_template("ftc/code/teleop.html", records=records)
 
 @app.route('/code/auton')
 def code_auton_page():
-    return render_template("ftc/code/auton.html")
+    records = fetch_data_from_db("code", "auton")
+    return render_template("ftc/code/auton.html", records=records)
 
 @app.route('/code/ftclib')
 def code_ftclib_page():
-    return render_template("ftc/code/ftclib.html")
+    records = fetch_data_from_db("code", "ftclib")
+    return render_template("ftc/code/ftclib.html", records=records)
 
 @app.route('/portfolios')
 def portfolios():
-    return render_template("ftc/portfolios/portfolios.html")
+    records = fetch_data_from_db("portfolios", "portfolios")
+    return render_template("ftc/portfolios/portfolios.html", records=records)
 
 
 if __name__ == '__main__':
--- db_modifier.sql
+++ db_modifier.sql
@@ -1,41 +1,14 @@
 -- Use this space to modify any databases
 -- Change the data source in the top right corner to the database you want to use
 
-CREATE TABLE auton
+CREATE TABLE portfolios
 (
     id                  INTEGER PRIMARY KEY AUTOINCREMENT,
-    title               TEXT NOT NULL,
-    author              TEXT NOT NULL,
-    preview_path        TEXT NOT NULL,
-    description         TEXT NOT NULL,
+    title               TEXT    NOT NULL,
+    file_path           TEXT    NOT NULL,
+    preview_path        TEXT    NOT NULL,
+    description         TEXT    NOT NULL,
     team_number         INTEGER NOT NULL,
-    onshape_link        TEXT NOT NULL,
     used_in_competition BOOLEAN NOT NULL,
-    years_used          TEXT NOT NULL
-);
-
-CREATE TABLE ftclib
-(
-    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
-    title               TEXT NOT NULL,
-    author              TEXT NOT NULL,
-    preview_path        TEXT NOT NULL,
-    description         TEXT NOT NULL,
-    team_number         INTEGER NOT NULL,
-    onshape_link        TEXT NOT NULL,
-    used_in_competition BOOLEAN NOT NULL,
-    years_used          TEXT NOT NULL
-);
-
-CREATE TABLE teleop
-(
-    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
-    title               TEXT NOT NULL,
-    author              TEXT NOT NULL,
-    preview_path        TEXT NOT NULL,
-    description         TEXT NOT NULL,
-    team_number         INTEGER NOT NULL,
-    onshape_link        TEXT NOT NULL,
-    used_in_competition BOOLEAN NOT NULL,
-    years_used          TEXT NOT NULL
+    years_used          TEXT    NOT NULL
 );
