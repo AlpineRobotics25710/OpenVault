70cebc9754de97472d2d594befaf656f264d14b1
PrathyushY <prathyushyeturi@gmail.com>
2025-01-29 11:29:53 -0500

    i think i made search functionality work in prod fingers crossed

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from bs4 import BeautifulSoup
 import uuid
 
 app = Flask(__name__)
-
-records = []
-curr_template = ""
+app.secret_key = "779650ac697181207529db19091dc55b93aa47a70ffbbe52d9cb8330c7b9ed4f"
 
 
 @app.context_processor
 def inject_active_route():
     # Provide the current route name to all templates
-    print(request.path)
-    return {'active_route': request.path}
+    #print(session["curr_template"])
+    return {'active_route': session["curr_template"]}
 
 
 # Generic function to fetch data from GitHub
 def fetch_data_from_github(section, sub_section):
-    global records
     base_url = "https://raw.githubusercontent.com/AlpineRobotics25710/OpenVaultFiles/refs/heads/main/ftc"
     github_page = requests.get(
         f"https://github.com/AlpineRobotics25710/OpenVaultFiles/tree/main/ftc/{section}/{sub_section}")
-    records = []
+    session["records"] = []
 
     if github_page.status_code == 200:
         soup = BeautifulSoup(github_page.content, "html.parser")
@@ -57,20 +54,31 @@ def fetch_data_from_github(section, sub_section):
                     record["used_in_comp"] = post_info_json["used-in-comp"]
                     record["onshape_link"] = post_info_json["onshape-link"]
 
-                records.append(record)
+                session["records"].append(record)
 
-    return records
+    return session["records"]
 
 
 @app.route('/search', methods=["GET", "POST"])
 def search():
-    global records, curr_template
+    if "curr_template" not in session:
+        return index()
+
+    if "records" not in session or session["records"] == []:
+        return render_template(session["curr_template"], records=session["records"])
+
+    curr_template = session["curr_template"]
+    records = session["records"]
+    #print("curr template:" + curr_template)
+    #print("records:" + str(records))
     if request.method == "POST":
         search_query = request.form.get("searchBox")
         if search_query == "":
             return render_template(curr_template, records=records)
         if search_query:
             search_query = search_query.lower()
+            #print("search query:" + search_query)
+            #print(records)
             filtered_records = [
                 record for record in records if
                 search_query in record["title"].lower() or
@@ -86,31 +94,29 @@ def search():
 
 @app.route('/')
 def index():
+    session["curr_template"] = "ftc/index.html"
     return render_template("ftc/index.html")
 
 
 @app.route('/cad/<category>')
 def render_cad_page(category):
-    global records, curr_template
-    records = fetch_data_from_github("cad", category)
-    curr_template = f"ftc/cad/{category}.html"
-    return render_template(curr_template, records=records)
+    session["records"] = fetch_data_from_github("cad", category)
+    session["curr_template"] = f"ftc/cad/{category}.html"
+    return render_template(session["curr_template"], records=session["records"])
 
 
 @app.route('/code/<category>')
 def render_code_page(category):
-    global records, curr_template
-    records = fetch_data_from_github("code", category)
-    curr_template = f"ftc/code/{category}.html"
-    return render_template(curr_template, records=records)
+    session["records"] = fetch_data_from_github("code", category)
+    session["curr_template"] = f"ftc/code/{category}.html"
+    return render_template(session["curr_template"], records=session["records"])
 
 
 @app.route('/portfolios')
 def portfolios():
-    global records, curr_template
-    records = fetch_data_from_github("portfolios", "portfolios")
-    curr_template = "ftc/portfolios/portfolios.html"
-    return render_template("ftc/portfolios/portfolios.html", records=records)
+    session["records"] = fetch_data_from_github("portfolios", "portfolios")
+    session["curr_template"] = "ftc/portfolios/portfolios.html"
+    return render_template(session["curr_template"], records=session["records"])
 
 
 if __name__ == '__main__':
